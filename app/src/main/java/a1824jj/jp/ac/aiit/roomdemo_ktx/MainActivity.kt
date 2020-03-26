293b26d0984d7d1e71e7c4cc8e6315edb70a1017
a1824jj <a1824jj@aiit.ac.jp>
2020-03-26 09:59:15 +0900

    project setup

---
+++ app/src/main/java/a1824jj/jp/ac/aiit/roomdemo_ktx/MainActivity.kt
@@ -0,0 +1,12 @@
+package a1824jj.jp.ac.aiit.roomdemo_ktx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
