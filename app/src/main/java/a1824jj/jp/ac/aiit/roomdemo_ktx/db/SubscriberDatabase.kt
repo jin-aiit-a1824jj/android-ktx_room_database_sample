22d6ee25b4239f632af9abaefc33f2e2db39ee52
a1824jj <a1824jj@aiit.ac.jp>
2020-03-26 10:23:33 +0900

    add Database class

---
+++ app/src/main/java/a1824jj/jp/ac/aiit/roomdemo_ktx/db/SubscriberDatabase.kt
@@ -0,0 +1,34 @@
+package a1824jj.jp.ac.aiit.roomdemo_ktx.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Subscriber::class], version = 1, exportSchema = false)
+abstract class SubscriberDatabase : RoomDatabase() {
+
+    abstract val subscriberDAO : SubscriberDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE : SubscriberDatabase? = null
+
+        fun getInstance(context: Context): SubscriberDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SubscriberDatabase::class.java,
+                        "subscriber_data_database")
+                        .build()
+                }
+
+             return instance
+            }
+        }
+
+     }
+
+}
